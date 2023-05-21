e602b938f5e1a6b4ca1dfdfc49afa65c72c3459c
mohaixiao <396367702@qq.com>
2023-05-21 23:11:35 +0800

    redux 持久化

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,8 +1,14 @@
-'use client';
-
+"use client";
+import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
-import { store } from "../store/store";
+import { store, persistor } from "../store/store";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,21 +1,32 @@
 "use client";
 
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import registerReducer from "../slices/registerSlice";
 import loginReducer from "@/slices/loginSlice";
 import userReducer from "@/slices/userSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage: storage,
+};
+
+const rootReducer = combineReducers({
+  register: registerReducer,
+  login: loginReducer,
+  user: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export function makeStore() {
   return configureStore({
-    reducer: {
-      register: registerReducer,
-      login: loginReducer,
-      user: userReducer,
-    },
+    reducer: persistedReducer,
   });
 }
 
 export const store = makeStore();
-
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
